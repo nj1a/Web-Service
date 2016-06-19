f77e99c1c2ba9f203ed6d60973e33e3571c622d9
njia95 <nathan.jia@mail.utoronto.ca>
2016-06-19 11:27:59 -0400

    add error checking code on server side

--- assets/scripts/server.js
+++ assets/scripts/server.js
@@ -2,10 +2,27 @@ var http = require('http'),
     fs = require('fs');
 
 http.createServer(function(req, res) {
-    if (req.url === "/text-articles") {
-        fs.readFile("../../nytimes.json", "utf8", function(err, data) {
-            res.end(data);
-        });
+    // handle errors on the request end
+    req.on('error', function(err) {
+        console.error(err);
+        res.statusCode = 400;
+        res.end();
+    });
+
+    // handle errors on the ressponse end
+    res.on('error', function(err) {
+        console.error(err);
+    });
+    
+    if (req.method === 'GET') {
+        if (req.url === '/text-articles') {
+            fs.readFile('../../nytimes.json', 'utf8', function(err, data) {
+                res.end(data);
+            });
+        } 
+    } else { // return page not found status
+        response.statusCode = 404;
+        response.end();
     }
 
 }).listen(8080);
