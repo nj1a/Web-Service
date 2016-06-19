7b032df4f7197cf281aa18eed3924c23911f7a5f
njia95 <nathan.jia@mail.utoronto.ca>
2016-06-18 21:10:16 -0400

    encapsulate GET insdie a class and set up a on-click button in html

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,19 +1,31 @@
+class REST {
+    allArticles() {
+        $.ajax({
+            url: "http:localhost:8080/",
+            type: "GET",
+            dataType: "json",  
+        }).done(function(data, textStatus, jqXHR) {
+            var $obj = data[0];
+            for (var $key in $obj) {
+
+                var $name = $("<h3/>", {
+                    text: $key
+                });
+                $("#topthree").append($name);
+            }
+
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            alert("Request failed: " + textStatus + " " + errorThrown);
+        });
+    }
+}
+
+
 $(document).ready(function() {
-    $.ajax({
-        url: "http:localhost:8080/",
-        type: "GET",
-        dataType: "json",  
-    }).done(function(data, textStatus, jqXHR) {
-        var $obj = data[0];
-        for (var $key in $obj) {
+    var r = new REST();
 
-            $name = $("<h3/>", {
-                text: $key
-            });
-            $("#topthree").append($name);
-        }
-
-    }).fail(function(jqXHR, textStatus, errorThrown) {
-        alert("Request failed: " + textStatus + " " + errorThrown);
+    $("#find").click(function() {
+        r.allArticles();
     });
 });
+
