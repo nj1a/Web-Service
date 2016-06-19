c4cbfcb4192c86e67a0475fca4492197d798fa7c
njia95 <nathan.jia@mail.utoronto.ca>
2016-06-19 16:28:17 -0400

    style arr articles in text

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,21 +6,25 @@ class NytimesApi {
             dataType: "json",  
         }).done(function(data, textStatus, jqXHR) {
             for (var i = 0; i < 10; i++) {
+                var $article = $("<article/>");
+
                 var $title = $("<h3/>")
                 .text("Title: " + data[i].title)
-                .appendTo("#display");
+                .appendTo($article);
 
                 var $abstract = $("<h3/>")
                 .text("Abstract: " + data[i].abstract)
-                .appendTo("#display");
+                .appendTo($article);
 
                 var $publishedDate = $("<h3/>")
                 .text("Published Date: " + data[i].published_date.slice(0, 10))
-                .appendTo("#display");
+                .appendTo($article);
 
                 var $snf = $("<h3/>")
                 .text("snf: " + data[i].short_url)
-                .appendTo("#display");
+                .appendTo($article);
+
+                $article.appendTo("#display");
             }
 
         }).fail(function(jqXHR, textStatus, errorThrown) {
